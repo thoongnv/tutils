f088760a1ad28e05d9874706e6801fba74286a7a
Nguyen Van Thong <thoongnv@gmail.com>
2019-02-25 21:04:43 +0700

    [IMP] - Search for the project in Gitlab

--- main.go
+++ main.go
@@ -37,25 +37,6 @@ func main() {
     gitlabAPIURL := v.GetString("gitlab.api_url")
     gitlabPrivateToken := v.GetString("gitlab.private_token")
 
-    // build request url
-    reqURL := gitlabAPIURL + "/projects?private_token=" + gitlabPrivateToken
-    client := &http.Client{Timeout: 10 * time.Second}
-
-    // get all projects
-    resp, err := client.Get(reqURL)
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer resp.Body.Close()
-    body, err := ioutil.ReadAll(resp.Body)
-
-    // mapping gitlab projects
-    var projects []Project
-    err = json.Unmarshal(body, &projects)
-    if err != nil {
-        log.Fatal(err)
-    }
-
     // get last folder name of current directory
     dir, err := os.Getwd()
 	if err != nil {
@@ -64,25 +45,40 @@ func main() {
     folders := strings.Split(dir, "/")
     folder := folders[len(folders)-1]
 
-    var found = false
-    for _, p := range projects {
-		if p.Name == folder {
-            found = true
-            // get last commit hash
-            cmd := exec.Command("git", "rev-parse", "HEAD")
-            commit, err := cmd.Output()
-            if err != nil {
-                log.Fatal(err)
-            }
-            commitURL := "[COMMIT](" + p.WebURL + "/commit/" + strings.TrimSuffix(string(commit), "\n") + ")"
+    // build request url
+    reqURL := gitlabAPIURL + "/projects?private_token=" +
+              gitlabPrivateToken + "&search=" + folder + "&simple=true"
+    client := &http.Client{Timeout: 10 * time.Second}
 
-            // copy to clipboard
-            clipboard.WriteAll(commitURL)
-            fmt.Println("Let's pasteeeeeeeeee ...")
-		}
+    // search for project
+    resp, err := client.Get(reqURL)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer resp.Body.Close()
+    body, err := ioutil.ReadAll(resp.Body)
+
+    // mapping gitlab project
+    var projects []Project
+    err = json.Unmarshal(body, &projects)
+    if err != nil {
+        log.Fatal(err)
     }
 
-    if found == false {
-        fmt.Println("Failed to generate commit URL ...")
+    if len(projects) == 1 {
+        // get last commit hash
+        cmd := exec.Command("git", "rev-parse", "HEAD")
+        commit, err := cmd.Output()
+        if err != nil {
+            log.Fatal(err)
+        }
+        commitURL := "[COMMIT](" + projects[0].WebURL + "/commit/" +
+                     strings.TrimSuffix(string(commit), "\n") + ")"
+
+        // copy to clipboard
+        clipboard.WriteAll(commitURL)
+        fmt.Println("Let's pasteeeeeeeeee ...")
+    } else {
+        fmt.Println("Not found project " + folder + "in Gitlab repository ...")
     }
 }
